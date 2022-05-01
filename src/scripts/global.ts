45a7b2f550bcfb3bfd4ec3588f061e0934684f0a
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-04-30 23:58:38 -0700

    feat: query for a pokemon based on name

--- src/scripts/global.ts
+++ src/scripts/global.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+interface Window {
+  pokedex?: Pokedex;
+}
+
+interface Pokedex {
+  fetchPokemonByName?: <T = { [name: string]: unknown }>(name: string) => Promise<T>;
+}
+
+(self => {
+  self.fetchPokemonByName = async function fetchPokemonByName<T>(name: string) {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
+      method: "GET",
+    });
+
+    return data.json() as Promise<T>;
+  };
+
+})(window.pokedex = window.pokedex || {});
