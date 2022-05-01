1c85250751617f892a17335568ba0b6642e2dd32
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-01 11:46:21 -0700

    fix: add additional data processing to return pokemon objects

--- src/scripts/global.ts
+++ src/scripts/global.ts
@@ -1,25 +1,29 @@
 (self => {
-  self.fetchPokemonByName = async function fetchPokemonByName<T>(name: string) {
+  const fetchPokemonByName = self.fetchPokemonByName = async function fetchPokemonByName(name) {
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
       method: "GET",
     });
 
-    return data.json() as Promise<T>;
+    return data.json() as Promise<Pokemon>;
   };
 
-  self.fetchPokemonByType = async function fetchPokemonByType<T>(type: string) {
-    const data = await fetch(`https://pokeapi.co/api/v2/type/${type}`, {
+  self.fetchPokemonByType = async function fetchPokemonByType(type) {
+    const pokeTypeResponse = await fetch(`https://pokeapi.co/api/v2/type/${type}`, {
       method: 'GET',
     });
 
-    return data.json() as Promise<T>;
+    const pokeTypeData: PokemonType = await pokeTypeResponse.json();
+
+    return Promise.all(pokeTypeData.pokemon.map(({ pokemon }) => fetchPokemonByName(pokemon.name)));
   };
 
-  self.fetchPokemonByAbility = async function fetchPokemonByAbility<T>(abilityName: string) {
-    const data = await fetch(`https://pokeapi.co/api/v2/ability/${abilityName}`, {
+  self.fetchPokemonByAbility = async function fetchPokemonByAbility(abilityName) {
+    const pokeAbilityResponse = await fetch(`https://pokeapi.co/api/v2/ability/${abilityName}`, {
       method: 'GET',
     });
 
-    return data.json() as Promise<T>;
+    const pokeAbilityData: PokemonAbility = await pokeAbilityResponse.json();
+
+    return Promise.all(pokeAbilityData.pokemon.map(({ pokemon }) => fetchPokemonByName(pokemon.name)));
   };
 })(window.pokedex = window.pokedex || {});
