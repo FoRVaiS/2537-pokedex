acb2fc2704f16de4f3fd4d1a03309402aff59e72
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-01 11:28:30 -0700

    build(typescript): create a type declaration file

--- src/scripts/global.ts
+++ src/scripts/global.ts
@@ -1,13 +1,3 @@
-interface Window {
-  pokedex?: Partial<Pokedex>;
-}
-
-interface Pokedex {
-  fetchPokemonByName: <T = { [name: string]: unknown }>(name: string) => Promise<T>;
-  fetchPokemonByType: <T = { [name: string]: unknown }>(type: string) => Promise<T>;
-  fetchPokemonByAbility: <T = { [name: string]: unknown }>(abilityName: string) => Promise<T>;
-}
-
 (self => {
   self.fetchPokemonByName = async function fetchPokemonByName<T>(name: string) {
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
