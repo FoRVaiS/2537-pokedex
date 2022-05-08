429091931ded11a07a316ac8c515632818ad80c8
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-07 20:38:27 -0700

    refactor: move namespaced functions back to global script

--- src/global.ts
+++ src/global.ts
@@ -1,3 +1,46 @@
 (() => {
+  (self => {
+    self.history = [];
+
+    function* generateHistoryId(): IterableIterator<number> {
+      let id = 0;
+
+      while(true) yield id++;
+    };
+
+    const idGenerator = generateHistoryId();
+
+    self.addResultToHistory = function addResultToHistory(result) {
+      self.history!.push({ id: idGenerator.next().value, ...result});
+    }
+
+    const fetchPokemonByName = self.fetchPokemonByName = async function fetchPokemonByName(id) {
+      const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+        method: "GET",
+      });
+
+      return [await (data.json() as Promise<Pokemon>)];
+    };
+
+    self.fetchPokemonByType = async function fetchPokemonByType(id) {
+      const pokeTypeResponse = await fetch(`https://pokeapi.co/api/v2/type/${id}`, {
+        method: 'GET',
+      });
+
+      const pokeTypeData: PokemonType = await pokeTypeResponse.json();
+
+      return (await Promise.all(pokeTypeData.pokemon.map(({ pokemon }) => fetchPokemonByName(pokemon.name)))).map(([result]) => result);
+    };
+
+    self.fetchPokemonByAbility = async function fetchPokemonByAbility(id) {
+      const pokeAbilityResponse = await fetch(`https://pokeapi.co/api/v2/ability/${id}`, {
+        method: 'GET',
+      });
+
+      const pokeAbilityData: PokemonAbility = await pokeAbilityResponse.json();
+
+      return (await Promise.all(pokeAbilityData.pokemon.map(({ pokemon }) => fetchPokemonByName(pokemon.name)))).map(([result]) => result);
+    };
+  })(window.pokedex = window.pokedex || {});
 
 })();
--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,48 +1,4 @@
 (() => {
-  (self => {
-    self.history = [];
-
-    function* generateHistoryId(): IterableIterator<number> {
-      let id = 0;
-
-      while(true) yield id++;
-    };
-
-    const idGenerator = generateHistoryId();
-
-    self.addResultToHistory = function addResultToHistory(result) {
-      self.history!.push({ id: idGenerator.next().value, ...result});
-    }
-
-    const fetchPokemonByName = self.fetchPokemonByName = async function fetchPokemonByName(id) {
-      const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-        method: "GET",
-      });
-
-      return [await (data.json() as Promise<Pokemon>)];
-    };
-
-    self.fetchPokemonByType = async function fetchPokemonByType(id) {
-      const pokeTypeResponse = await fetch(`https://pokeapi.co/api/v2/type/${id}`, {
-        method: 'GET',
-      });
-
-      const pokeTypeData: PokemonType = await pokeTypeResponse.json();
-
-      return (await Promise.all(pokeTypeData.pokemon.map(({ pokemon }) => fetchPokemonByName(pokemon.name)))).map(([result]) => result);
-    };
-
-    self.fetchPokemonByAbility = async function fetchPokemonByAbility(id) {
-      const pokeAbilityResponse = await fetch(`https://pokeapi.co/api/v2/ability/${id}`, {
-        method: 'GET',
-      });
-
-      const pokeAbilityData: PokemonAbility = await pokeAbilityResponse.json();
-
-      return (await Promise.all(pokeAbilityData.pokemon.map(({ pokemon }) => fetchPokemonByName(pokemon.name)))).map(([result]) => result);
-    };
-  })(window.pokedex = window.pokedex || {});
-
   const clearResultsSection = function clearResultsSection(): void {
     const resultsRef: HTMLElement = document.querySelector("section[region='results'] > .u-center-evenly-spaced")!;
 
@@ -141,9 +97,9 @@
   };
 
   const modeOptions: { [key in keyof typeof enumModeOptions]: fetchPokemonFn } = {
-    "Name": window.pokedex.fetchPokemonByName!,
-    "Ability": window.pokedex.fetchPokemonByAbility!,
-    "Type": window.pokedex.fetchPokemonByType!,
+    "Name": window.pokedex!.fetchPokemonByName!,
+    "Ability": window.pokedex!.fetchPokemonByAbility!,
+    "Type": window.pokedex!.fetchPokemonByType!,
   };
 
   // Populate mode selector with with option keys
