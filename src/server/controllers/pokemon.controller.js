3bc932fbc97b293413372b9c289ad42fd89e819f
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-13 11:07:14 -0700

    refactor: break down fetchPokemon controller into smaller functions

--- src/server/controllers/pokemon.controller.js
+++ src/server/controllers/pokemon.controller.js
@@ -3,27 +3,30 @@ const { PokemonModel } = require("../models/pokemon.model");
 const { isNumericalString } = require("../../utils/isNumericalString");
 const { isAlphabeticalString } = require("../../utils/isAlphabeticalString");
 
+const PokemonNotFoundErrorPayload = {
+  success: false,
+  data: {
+      msg: 'Failed to find pokemon because it either does not exist or an internal server error has occurred.',
+  }
+}
+
 const fetchPokemon = async (req, res) => {
   const { id } = req.params;
 
-  let pokemon = null;
-
-  if (isNumericalString(id)) pokemon = (await PokemonModel.find({ id }, { _id: 0, __v: 0 }))[0];
-  else if (isAlphabeticalString(id)) pokemon = (await PokemonModel.find({ name: id }, { _id: 0, __v: 0 }))[0];
+  if (isNumericalString(id)) await fetchPokemonById(id, req, res);
+  else if (isAlphabeticalString(id)) await fetchPokemonByName(id, req, res);
   else return res.status(400).json({
     success: false,
     data: {
       msg: 'The :id parameter must only contain numbers or only contain letters.',
     },
   });
+};
 
+const fetchPokemonById = async (id, req, res) => {
+  const [pokemon] = await PokemonModel.find({ id }, { _id: 9, __v: 0 });
 
-  if (!pokemon) return res.status(500).json({
-    success: false,
-    data: {
-      msg: 'Failed to find pokemon because it either does not exist or an internal server error has occurred.',
-    },
-  });
+  if (!pokemon) return res.status(500).json(PokemonNotFoundErrorPayload);
 
   return res.status(200).json({
     success: true,
@@ -31,4 +34,15 @@ const fetchPokemon = async (req, res) => {
   });
 };
 
-module.exports = { fetchPokemon };
+const fetchPokemonByName = async (name, req, res) => {
+  const [pokemon] = await PokemonModel.find({ name }, { _id: 0, __v: 0 });
+
+  if (!pokemon) return res.status(500).json(PokemonNotFoundErrorPayload);
+
+  return res.status(200).json({
+    success: true,
+    data: pokemon,
+  });
+};
+
+module.exports = { fetchPokemon, fetchPokemonById, fetchPokemonByName };
