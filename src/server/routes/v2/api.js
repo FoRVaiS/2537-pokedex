af4b7908ff184d86b5a552ff7710673990d922a0
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-21 17:36:36 -0700

    feat: create post route for user registration

--- src/server/routes/v2/api.js
+++ src/server/routes/v2/api.js
@@ -4,6 +4,7 @@ const pokemonController = require('../../controllers/pokemon.controller');
 const typeController = require('../../controllers/type.controller');
 const abilityController = require('../../controllers/ability.controller');
 const timelineController = require('../../controllers/timeline.controller');
+const userController = require('../../controllers/user.controller');
 
 const createV2Router = () => {
   const router = Router();
@@ -16,6 +17,7 @@ const createV2Router = () => {
   router.get('/timeline/event/:name', timelineController.fetchEvent);
   router.post('/timeline/capture', timelineController.captureEvent);
   router.post('/timeline/remove', timelineController.deleteEvent);
+  router.post('/user/register', userController.createUser);
 
   return router;
 };
