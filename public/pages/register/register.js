6e2b1ca50c8c28ea981048d712c7d665ecaa80db
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-21 23:38:26 -0700

    feat: edit registration script to include new fields

--- public/pages/register/register.js
+++ public/pages/register/register.js
@@ -1,7 +1,7 @@
 (async () => {
   const { query } = window.pokedex;
 
-  const sendRegisterRequest = (username, password) => query('/api/v2/user/register', {
+  const sendRegisterRequest = ({ username, password, firstName, lastName, age, gender }) => query('/api/v2/user/register', {
     method: 'post',
     headers: {
       'Content-Type': 'application/json',
@@ -9,11 +9,19 @@
     body: JSON.stringify({
       username: username.trim(),
       password,
+      firstName: firstName.trim(),
+      lastName: lastName.trim(),
+      age,
+      gender: gender.trim(),
     }),
   });
 
   const usernameField = document.querySelector('input[name=\'username\']');
   const passwordField = document.querySelector('input[name=\'password\']');
+  const firstNameField = document.querySelector('input[name=\'first-name\']');
+  const lastNameField = document.querySelector('input[name=\'last-name\']');
+  const ageField = document.querySelector('input[name=\'age\']');
+  const genderField = document.querySelector('select[name=\'gender\']');
   const alertDialogue = document.querySelector('p.form__alert');
   const registerBtn = document.querySelector('input[type=\'submit\']');
   let lastTimeout = null;
@@ -21,7 +29,14 @@
   registerBtn.onclick = e => {
     e.preventDefault();
 
-    sendRegisterRequest(usernameField.value, passwordField.value)
+    sendRegisterRequest({
+      username: usernameField.value,
+      password: passwordField.value,
+      firstName: firstNameField.value,
+      lastName: lastNameField.value,
+      age: ageField.value,
+      gender: genderField.value,
+    })
       .then(results => {
         if (!results.success) {
           clearTimeout(lastTimeout);
