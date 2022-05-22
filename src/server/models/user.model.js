ab2d79f11652ea8ba4a1cdf5daebf7f2c686609a
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-21 23:40:36 -0700

    feat: add new user information to user model

--- src/server/models/user.model.js
+++ src/server/models/user.model.js
@@ -3,6 +3,10 @@ const { model, Schema } = require('mongoose');
 const UserModel = model('user', new Schema({
   username: String,
   password: String,
+  firstName: String,
+  lastName: String,
+  age: Number,
+  gender: String,
   roles: [String],
 }));
 
