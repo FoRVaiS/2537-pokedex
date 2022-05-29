af37f61b26c5d35dada0f2cc5e90e84ed21105ca
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-29 09:54:16 -0700

    refactor: remove local pokemon api

--- src/server/routes/v2/api.js
+++ src/server/routes/v2/api.js
@@ -1,8 +1,5 @@
 const { Router } = require('express');
 
-const pokemonController = require('../../controllers/pokemon.controller');
-const typeController = require('../../controllers/type.controller');
-const abilityController = require('../../controllers/ability.controller');
 const timelineController = require('../../controllers/timeline.controller');
 const userController = require('../../controllers/user.controller');
 const cartController = require('../../controllers/cart.controller');
@@ -10,10 +7,6 @@ const cartController = require('../../controllers/cart.controller');
 const createV2Router = () => {
   const router = Router();
 
-  router.get('/pokemon/:id', pokemonController.fetchPokemon);
-  router.get('/pokemon', pokemonController.fetchPokemon);
-  router.get('/type/:id', typeController.fetchType);
-  router.get('/ability/:id', abilityController.fetchAbility);
   router.get('/user/timeline/event', timelineController.fetchEvents);
   router.get('/user/timeline/event/:name', timelineController.fetchEvent);
   router.post('/user/timeline/capture', timelineController.captureEvent);
