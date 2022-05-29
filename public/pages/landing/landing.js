c1698f35f1a2b59717d83dbdc8737f9b12a7f5c6
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-05-29 08:45:28 -0700

    refactor: modify all pokemon related urls to point to pokeapiA

--- public/pages/landing/landing.js
+++ public/pages/landing/landing.js
@@ -6,7 +6,7 @@
   
   const verifyDreamworldSprite = pokemon => pokemon.sprites.other.dream_world.front_default !== null;
   const loadRandomDreamworldPokemon = async size => {
-    const results = await window.pokedex.query(`/api/v2/pokemon?limit=${size}&offset=${randomIntBetweenRange(0, 868 - size)}`);
+    const results = await window.pokedex.query(`http://pokeapi.co/api/v2/pokemon?limit=${size}&offset=${randomIntBetweenRange(0, 868 - size)}`);
 
     // Check that all the pokemon have sprites
     return results.data.filter(_pokemon => !verifyDreamworldSprite(_pokemon)).length === 0
