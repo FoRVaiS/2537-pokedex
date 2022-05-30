3e40544f5c55229946ae38cdaee16e43255a50ec
Benjamin Chiang <1572440+FoRVaiS@users.noreply.github.com>
2022-05-29 17:22:45 -0700

    feat: create edit route

--- public/pages/admin-dashboard/admin-dashboard.js
+++ public/pages/admin-dashboard/admin-dashboard.js
@@ -1,11 +1,13 @@
 (() => {
   const { query, getRequiredValueFrom } = window.pokedex;
 
+  let _id = null;
+
   const handleEdit = id => window.location.href = `/dashboard?id=${id}`;
 
   const handleEditSubmit = async e => {
     e.preventDefault();
-    
+
     const username = getRequiredValueFrom('input[name=\'username\']', String);
     const password = getRequiredValueFrom('input[name=\'password\']', String);
     const firstName = getRequiredValueFrom('input[name=\'first-name\']', String);
@@ -13,13 +15,20 @@
 
     if ([username, password, firstName, lastName].includes(null)) return null;
 
-    const { success } = await query('/user/edit', { username, password, firstName, lastName });
+    const { success } = await query('/api/v2/user/edit', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ _id, username, password, firstName, lastName }),
+    });
 
     if (success) window.location.href = '/dashboard';
   };
 
   const userEditBtns = document.querySelectorAll('.in-edit');
   userEditBtns.forEach(editBtn => {
+    _id = editBtn.getAttribute('data-user-id');
     editBtn.addEventListener('click', handleEdit.bind(null, editBtn.getAttribute('data-user-id')));
   });
 
